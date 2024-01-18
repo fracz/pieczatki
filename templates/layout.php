d2656812506f26b11388b85ebd15993ef505e24d
Wojciech Frącz <fraczwojciech@gmail.com>
2024-01-18 21:48:36 +0100

    Meta

--- templates/layout.php
+++ templates/layout.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <title><?= $title ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="Free HTML Templates" name="keywords">
-    <meta content="Free HTML Templates" name="description">
+    <meta content="Katalog polskich pieczątek turystycznych" name="description">
 
     <!-- Favicon -->
     <link href="/img/favicon.ico" rel="icon">
